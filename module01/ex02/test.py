e8ec8debe8be14d48ad54517009ee4e760bc9bcc
NachoDonaire <nacho23donaire@gmail.com>
2023-03-15 14:11:43 +0100

    vector finished

--- module01/ex02/test.py
+++ module01/ex02/test.py
@@ -8,17 +8,25 @@ def printeito(v, n):
 
 
 if __name__ == "__main__":
-    v = Vector([[3.], [2.], [4.]])
+    v = Vector(3)
     printeito(v, 1)
 
-    v2 = Vector([[3., 2., 4.]])
+    v2 = Vector([[1., 2., 1.]])
     printeito(v2, 2)
     
-    v3 = Vector([[3.], [2.], [1.]])
+    v3 = Vector((1, 4))
     printeito(v3, 3)
 
     v4 = Vector([[3., 1., 4.]])
     printeito(v4, 4)
+
+
+    dotander = v.dot(v3)
+    print("Esto es el producto escalar  de v con v3, vectores columna: " + str(dotander))
+
+    dotis = v2.dot(v4)
+    print("Esto es el producto escalar de v2 con v4, vectores fila: " + str(dotis))
+    print()
 
     dot = v2.T()
     print("Esto es la traspuesta de v2, un vector fila: " + str(dot))
@@ -39,3 +47,22 @@ if __name__ == "__main__":
 
     resta2 = v2 - v4
     print("Esto es la resta de v2 y v4, vectores fila" + str(resta2))
+    print()
+
+    prod = v2 * v
+    print("Producto entre v2 y v, fila y columna" + str(prod))
+
+    prod2 = v4 * v3
+    print("Producto entre v4 y v3, fila y columna" + str(prod2))
+
+    prod3 = v4 * 2.
+    print("Producto entre v4 y 2, fila escalar" + str(prod3))
+
+    prod4 = v * 2.
+    print("Producto entre v y 2, columna escalar" + str(prod4))
+    
+    q1 = v3 / 2.
+    print("Cociente entre v3 y 2, columna escalar" + str(q1))
+
+    q2 = v2 / 2.
+    print("Cociente entre v2 y 2, fila escalar" + str(q2))
--- module01/ex02/vector.py
+++ module01/ex02/vector.py
@@ -1,34 +1,59 @@
 class   Vector:
     def __init__(self, arg):
-        if not type(arg) == list:
-            print("error: vector must initialized as a list")
-            return
+        if type(arg) == tuple:
+            if arg[0] < arg[1]:
+                tucker = []
+                for element in range(arg[0], arg[1]):
+                    tuckercin = [[]]
+                    tuckercin[0] = float(element)
+                    tucker.append(tuckercin)
+                self.values = tucker
+                self.shape = (len(range(arg[0], arg[1])), 1)
+            else:
+                print("Wrong tuple initializing vector")
+                return
 
+        elif type(arg) == int:
+            tucker = []
+            if (arg < 3):
+                print("error: if initialize with int, arg must be more than 3")
+                return 
+            for element in range(arg):
+                tuckercin = [[]]
+                tuckercin[0] = float(element)
+                tucker.append(tuckercin)
+            self.values = tucker
+            self.shape = (len(range(arg)), 1)
+
+        elif type(arg) == list:
         #row vector
-        if (len(arg) > 1):
-            for a in arg:
-                if type(a) != list:
-                    print("error: vector must be initialized as a list")
-                    return
-                for n in a:
-                    if type(n) != float:
-                        print("error: vector must be float")
+            if (len(arg) > 1):
+                for a in arg:
+                    if type(a) != list:
+                        print("error: vector must be initialized as a list")
                         return
-            self.values = arg
-            self.shape = (len(arg), 1)
+                    for n in a:
+                        if type(n) != float:
+                            print("error: vector must be float")
+                            return
+                self.values = arg
+                self.shape = (len(arg), 1)
 
         #column vector
-        elif (len(arg) == 1):
-            for a in arg:
-                if type(a) != list:
-                    print("error: vector must be initialized as a list")
-                    return
-                for n in a:
-                    if not type(n) == float:
-                        print("error: vector must be float")
+            elif (len(arg) == 1):
+                for a in arg:
+                    if type(a) != list:
+                        print("error: vector must be initialized as a list")
                         return
-            self.values = arg
-            self.shape = (1, len(arg[0]))
+                    for n in a:
+                        if not type(n) == float:
+                            print("error: vector must be float")
+                            return
+                self.values = arg
+                self.shape = (1, len(arg[0]))
+        else:
+            print("error: wrong argument, only admits tuple, int, [[]]")
+            return
 
     def dot(self, v2):
         if not (self.shape == v2.shape):
@@ -90,3 +115,61 @@ class   Vector:
                     aux[0] = e1 - e2
                     suma.append(aux)
             return suma
+
+    def __mul__(self, k):
+        if type(k) == Vector:
+            if not self.shape[0] == 1:
+                print("first vector in product must be row vector")
+                return
+            if not k.shape[1] == 1:
+                print("second vector must be column vector")
+                return
+            prod = []
+            aux = []
+            for row in k.values:
+                for e in row:
+                    aux.append(e)
+            for e1, e2 in zip(self.values[0], aux):
+                prody = [[]]
+                prody[0] = e1 * e2
+                prod.append(prody)
+            return (prod)
+
+        elif type(k) == float:
+            if not self.shape[1] == 1:
+                prod = []
+                for e in self.values[0]:
+                    prod.append(e * k)
+                return prod
+            else:
+                prod = []
+                for row in self.values:
+                    for e in row:
+                        prody = [[]]
+                        prody[0] = e * k
+                        prod.append(prody)
+                return prod
+        else:
+            print("Product defined only between vectors and vector escalar")
+            return
+    def __truediv__(self, k):
+        if type(k) == float:
+            if (k == 0):
+                print("Quotient no defined by zero")
+                return
+            if not self.shape[1] == 1:
+                prod = []
+                for e in self.values[0]:
+                    prod.append(e / k)
+                return prod
+            else:
+                prod = []
+                for row in self.values:
+                    for e in row:
+                        prody = [[]]
+                        prody[0] = e / k
+                        prod.append(prody)
+                return prod
+        else:
+            print("Quotient defined for vector and escalar, exclusively")
+            return
