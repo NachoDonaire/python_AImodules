58ff981e6e7a9c419e7c3a3f6c4515d8c449b356
NachoDonaire <nacho23donaire@gmail.com>
2023-03-17 19:00:50 +0100

    ex02 module02 done

--- module02/ex02/logger.py
+++ module02/ex02/logger.py
@@ -1,21 +1,34 @@
 import time
+import functools
 from random import randint
 import os
 
 fd = open("machile.log", mode='w')
 
-def timeit
-
 def log(function):
-    action = "eating as a p"
-    tucker = " " + str(action)
-    final_spaces = 20 - len(tucker)
-    complement_s = str(tucker) + str(" " * final_spaces)
-    time = 4
-    texetx = "[ exec-time = {:.3f} ms ]".format(time)
-    s = "(cmaxime)Running:" +  str(complement_s) + str(texetx)
-    fd.write("(cmaxime)Running:\n" )
-    return function
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        retorno = function(*args, **kwargs)
+        end_time = time.time()
+        if (function.__name__ == "start_machine"):
+            action = "Starting machine"
+        elif (function.__name__ == "boil_water"):
+            action = "Boiling water"
+        elif function.__name__ == "make_coffee":
+            action = "Preparing coffee"
+        elif function.__name__ == "add_water":
+            action = "Adding water"
+        else:
+            action = "Not recognized action"
+        tucker = " " + str(action)
+        final_spaces = 20 - len(tucker)
+        complement_s = str(tucker) + str(" " * final_spaces)
+        times = end_time - start_time
+        texetx = "[ exec-time = {:.3f} ms ]".format(times)
+        s = "(" + str(os.getlogin()) + ")Running:" +  str(complement_s) + str(texetx)
+        fd.write(str(s) + "\n")
+        return retorno
+    return wrapper
 
 class CoffeeMachine():
     water_level = 100
